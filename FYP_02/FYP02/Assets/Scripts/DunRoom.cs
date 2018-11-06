c2646830165e520ac71407c95797efda894e2577
Samuel <bluecubestuff@gmail.com>
2018-11-06 18:49:03 +0800

    Initial Commit

---
+++ FYP_02/FYP02/Assets/Scripts/DunRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DunRoom : MonoBehaviour {
+
+    public int xPos; 
+    public int yPos;  
+    public int roomWidth;
+    public int roomHeight;
+    public Direction enteringCorridor;    // The direction of the corridor that is entering this room.
+                   
+   // public void PlaceRoom(IntRange widthRange, IntRange heightRange, int columns, int rows)
+
+}
