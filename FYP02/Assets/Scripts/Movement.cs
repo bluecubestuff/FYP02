366653f585a7d11f7a93eb381f021cae20e76cbb
Samuel <bluecubestuff@gmail.com>
2018-11-11 16:11:46 +0800

    Room generation, movement

---
+++ FYP02/Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+
+    private Vector3 forward = new Vector3(0, 1, 0);
+    private Vector3 right = new Vector3(1, 0, 0);
+
+    private Transform cameraTransform;
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            //rigidBody.AddForce(Vector3.up * moveSpeed);
+        transform.position += forward * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            //rigidBody.AddForce(-Vector3.right * moveSpeed);
+        transform.position -= right * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            //rigidBody.AddForce(-Vector3.up * moveSpeed);
+        transform.position -= forward * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            //rigidBody.AddForce(Vector3.right * moveSpeed);
+        transform.position += right * moveSpeed * Time.deltaTime;
+
+        rigidBody.MovePosition(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        cameraTransform.position = new Vector3(transform.position.x, transform.position.y - 1f, cameraTransform.position.z);
+    }
+}
---
+++ FYP02/Assets/Scripts/RoomGen/RoomTemplates.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+
+	public GameObject[] bottomRooms;
+	public GameObject[] topRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+
+	public GameObject closedRoom;
+	public GameObject boss;
+	public float waitTime;
+
+    // Store the rooms generated
+	public List<GameObject> rooms;
+
+	private bool spawnedBoss;
+
+	void Update(){
+        // might move this to start
+		if(waitTime <= 0 && spawnedBoss == false){
+			for (int i = 0; i < rooms.Count; i++) {
+				if(i == rooms.Count-1){
+					Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+					spawnedBoss = true;
+				}
+			}
+		} else {
+			waitTime -= Time.deltaTime;
+		}
+	}
+}
